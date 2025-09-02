92cc83e4e6d861ad173f450ba7053f6bc66e9543
SergioGBosco <sergiogibosco@gmail.com>
2025-09-02 14:08:30 +0200

    feat: add array from axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { useState } from "react"
-
+import axios from "axios"
 function App() {
   const [actorsList, SetActorList] = useState([])
   const [actressesList, SetActressesList] = useState([])
+
+
+  // Sezione Attrici
+  axios.get("https://lanciweb.github.io/demo/api/actresses/").then(resp => {
+    console.log(resp.data)
+  })
 
 
   return (
