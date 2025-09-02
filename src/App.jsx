4a8da7558e1329389e66663631e1cb848b0b2d8d
SergioGBosco <sergiogibosco@gmail.com>
2025-09-02 14:28:59 +0200

    feat: add card for show it video

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,33 @@ function App() {
 
   // Sezione Attrici
   axios.get("https://lanciweb.github.io/demo/api/actresses/").then(resp => {
-    console.log(resp.data)
+    // console.log(resp.data)
+    SetActressesList(resp.data)
+    // console.log(actressesList)
   })
 
 
   return (
-    <>
-      <div>esercizio di oggi Lista attori
+    <div className="container">
+      <div className="row">
+        {actressesList.map((actresses) => {
+          return (
+            <div div className="col-33" key={actresses.id}>
+              <div className="card">
+                <img src={actresses.image} alt={actresses.name} />
+                <div className="description">
+                  <span>{actresses.name}</span>
+                  <span>{actresses.birth_year}</span>
+                  <span>{actresses.nationality}</span>
+                  <span>{actresses.awards}</span>
+                  <p>{actresses.biography}</p>
+                </div>
+              </div>
+            </div>
+          )
+        })}
       </div>
-    </>
+    </div >
   )
 }
 
