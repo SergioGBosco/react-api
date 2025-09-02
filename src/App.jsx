a14c0942dc5f4c1a24a4b5e90f05f6a716f61236
SergioGBosco <sergiogibosco@gmail.com>
2025-09-02 16:47:50 +0200

    feat: add input for filtered array and show it

--- src/App.jsx
+++ src/App.jsx
@@ -6,36 +6,61 @@ function App() {
   const [actorsList, SetActorList] = useState([])
   const [actressesList, SetActressesList] = useState([])
   const [fullList, SetfullList] = useState([])
+  const [searchActors, SetSearchActors] = useState(``)
+  const [filteredActorsList, SetFilterredActorsList] = useState(``)
 
 
   // Sezione Attrici
-  const genereteActos = () => {
+  const generateActos = () => {
     axios.get("https://lanciweb.github.io/demo/api/actresses/").then(resp => {
       // console.log(resp.data)
       SetActressesList(resp.data)
       // console.log(actressesList)
     });
   };
-  useEffect(genereteActos, [])
-  console.log(actorsList)
+  useEffect(generateActos, [])
+
   //Sezioni Attori
-  const genereteActresses = () => {
+  const generateActresses = () => {
     axios.get("https://lanciweb.github.io/demo/api/actors/").then(resp => {
       SetActorList(resp.data)
     });
   };
-  useEffect(genereteActresses, [])
-  console.log(actressesList)
+  useEffect(generateActresses, [])
 
-  const allActors = [...actorsList, ...actressesList]
-  // SetfullList(allActors)
+  // Array di tutti gli attori 
 
+  const generetateFullList = () => {
+    const allActors = [...actorsList, ...actressesList]
+    SetfullList(allActors)
+  };
+
+  //array per filtraggio
+  const filteredList = fullList.filter(act => {
+    return (
+      act.name.toLowerCase().includes(searchActors)
+    )
+  });
+
+  const handlesearch = (e) => {
+    SetSearchActors(e.target.value)
+  }
+
+
+  useEffect(generetateFullList, [actorsList, actressesList])
+  console.log(fullList)
   return (
     <div className="container">
       <div className="row">
-        {/* {allActors.map((act) => {
+        <div className="col-100">
+          <h1>ATTORI FAMOSI</h1>
+          <input type="text" className="search" placeholder="Cerca Attore il tuo attore preferito..." value={searchActors} onChange={handlesearch} />
+        </div>
+      </div>
+      <div className="row">
+        {filteredList.map((act, index) => {
           return (
-            <div className="col-33" key={act.id}>
+            <div className="col-33" key={index}>
               <div className="card">
                 <div className="card-img">
                   <img src={act.image} alt={act.name} />
@@ -52,18 +77,19 @@ function App() {
               </div>
             </div>
           )
-        })} */}
-        {actressesList.map((actresses) => {
+        })}
+        {/* {actressesList.map((actresses) => {
           return (
             <ActressesList key={actresses.id} actresses={actresses} />
-          )
-        })},
-
-        {actorsList.map((actors) => {
+            )
+            })},
+            
+            {actorsList.map((actors) => {
           return (
             <ActorsList key={actors.id} actors={actors} />
-          )
-        })}
+            )
+            })} */}
+
       </div>
     </div >
   )
